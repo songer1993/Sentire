fa584f72ec7e3234da8a1b1f42b553dfd89a5ef7
songer1993 <songer1993@gmail.com>
2016-04-06 08:47:45 +0100

    4th

--- app/src/main/java/com/project/songer1993/sentire/DesignLightPatterns.java
+++ app/src/main/java/com/project/songer1993/sentire/DesignLightPatterns.java
@@ -1,23 +1,39 @@
 package com.project.songer1993.sentire;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+import com.github.channguyen.rsv.RangeSliderView;
 import com.nightonke.boommenu.BoomMenuButton;
 import com.nightonke.boommenu.Types.BoomType;
 import com.nightonke.boommenu.Types.ButtonType;
 import com.nightonke.boommenu.Types.PlaceType;
 import com.nightonke.boommenu.Util;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
+import java.util.List;
 
 public class DesignLightPatterns extends AppCompatActivity {
 
@@ -25,6 +41,18 @@ public class DesignLightPatterns extends AppCompatActivity {
     private ActionBar mActionBar;
     private boolean init = false;
     private Context mContext;
+
+
+    private Button btnPlay, btnAdd, btnClear;
+    boolean played = false;
+
+    private PieChart mPieChart;
+    private int mColor = 0xffffffff;
+    private int mNumber = 0;
+    private float mDuration = 0;
+
+    private SeekBar sbDuration;
+    private float range = 3f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +63,119 @@ public class DesignLightPatterns extends AppCompatActivity {
 
         mContext = this;
         mActionBar = getSupportActionBar();
+        mActionBar.setElevation(0);
         mActionBar.setDisplayShowHomeEnabled(false);
         mActionBar.setDisplayShowTitleEnabled(false);
         LayoutInflater mInflater = LayoutInflater.from(this);
-
-        View mCustomView = mInflater.inflate(R.layout.my_action_bar, null);
+        final View mCustomView = mInflater.inflate(R.layout.my_action_bar, null);
         TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.title_text);
         mTitleTextView.setText("Light Patterns");
-
         boomMenuButtonInActionBar = (BoomMenuButton) mCustomView.findViewById(R.id.boom);
-
         mActionBar.setCustomView(mCustomView);
         mActionBar.setDisplayShowCustomEnabled(true);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            //((Toolbar) mCustomView.getParent()).setContentInsetsAbsolute(0,0);
-        }
+
+        btnAdd = (Button)findViewById(R.id.btnAdd3);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ColorPickerDialogBuilder
+                        .with(mContext)
+                        .setTitle("Choose color")
+                        .initialColor(mColor)
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .density(12)
+                        .setPositiveButton("ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                mColor = selectedColor;
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                                LayoutInflater inflater = getLayoutInflater();
+                                View v = inflater.inflate(R.layout.my_duration_dialog, null);
+                                builder.setView(v)
+                                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                mNumber += 1;
+                                                mPieChart.addPieSlice(new PieModel((new Integer(mNumber)).toString(), mDuration, mColor));
+                                                mDuration = 0;
+                                            }
+                                        })
+                                .setTitle("Duration");
+
+                                SeekBar sbDuration = (SeekBar)v.findViewById(R.id.sbDuration);
+                                sbDuration.setMax(100);
+                                sbDuration.setProgress(0);
+                                sbDuration.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                                    @Override
+                                    public void onStopTrackingTouch(SeekBar seekBar) {
+                                        // TODO Auto-generated method stub
+
+                                    }
+
+                                    @Override
+                                    public void onStartTrackingTouch(SeekBar seekBar) {
+                                        // TODO Auto-generated method stub
+
+                                    }
+
+                                    @Override
+                                    public void onProgressChanged(SeekBar seekBar, int progress,
+                                                                  boolean fromUser) {
+                                        // TODO Auto-generated method stub
+                                        mDuration = progress * range / 100;
+                                    }
+                                });
+
+                                builder.create();
+                                builder.show();
+
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .showColorEdit(false)
+                        .build()
+                        .show();
+
+            }
+        });
+
+        btnPlay = (Button)findViewById(R.id.btnPlay3);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(played){
+                    played = false;
+                    btnPlay.setText("Play");
+                    btnPlay.setTextColor(getApplication().getResources().getColor(R.color.secondaryText));
+                }
+                else {
+                    played = true;
+                    btnPlay.setText("Save?");
+                    btnPlay.setTextColor(getApplication().getResources().getColor(R.color.colorAccent));
+                }
+            }
+        });
+
+        btnClear = (Button)findViewById(R.id.btnClear3);
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnPlay.setText("Play");
+                btnPlay.setTextColor(getApplication().getResources().getColor(R.color.colorPrimary));
+                played = false;
+            }
+        });
+
+        mPieChart = (PieChart) findViewById(R.id.piechart);
+        mPieChart.setInnerValueUnit("ms");
+
     }
 
 
